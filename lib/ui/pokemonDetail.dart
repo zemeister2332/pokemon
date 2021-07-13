7fb75df88b1e591c007eab0c00053461c3ad741b
zemeister2332 <noyanbayci2332@gmail.com>
2021-07-13 11:01:07 +0500

    second branch

--- lib/ui/pokemonDetail.dart
+++ lib/ui/pokemonDetail.dart
@@ -17,92 +17,216 @@ class PokemonDetail extends StatelessWidget {
         elevation: 0,
         title: Text(kelganPoke.name.toString()),
       ),
-      body: Stack(
-        children: [
-          Positioned(
-            width: MediaQuery.of(context).size.width - 40,
-            height: MediaQuery.of(context).size.height - 300,
-            top: 20,
-            right: 20,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              elevation: 1,
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          if (orientation == Orientation.portrait) {
+            return stackWidgetPortrait(context);
+          } else {
+            return stackWidgetLandscape(context);
+          }
+        },
+      ),
+    );
+  }
+
+  Widget stackWidgetLandscape(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          width: MediaQuery.of(context).size.width - 40,
+          height: MediaQuery.of(context).size.height - 100,
+          top: 20,
+          right: 20,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            elevation: 1,
+          ),
+        ),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Hero(
+            tag: kelganPoke.img,
+            child: Image.network(
+              kelganPoke.img,
+              fit: BoxFit.contain,
+              width: 250.0,
             ),
           ),
-          Align(
-            alignment: Alignment.topCenter,
-            child: Hero(
-              tag: kelganPoke.img,
-              child: Image.network(
-                kelganPoke.img,
-                fit: BoxFit.contain,
-                width: 200.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                "${kelganPoke.candy} Landscape",
+                style: TextStyle(
+                    fontSize: 25.0,
+                    backgroundColor: Colors.grey.withOpacity(0.2)),
               ),
             ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Align(
-                alignment: Alignment.center,
-                child: Text(
-                  "${kelganPoke.candy}",
-                  style: TextStyle(fontSize: 25.0),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: chiplar(),
-              ),
-              kelganPoke.nextEvolution != null
-                  ? Padding(
-                      padding: const EdgeInsets.all(38.0),
-                      child: ListTile(
-                        trailing: Icon(Icons.arrow_forward),
-                        title: Text("Next Level"),
-                        subtitle:
-                            Text("${kelganPoke.nextEvolution[0].nameLevel}"),
-                      ),
-                    )
-                  : Text("There is no Next Evolution"),
-              Padding(
-                padding: const EdgeInsets.only(top: 48.0),
-                child: Container(
-                  height: 100.0,
-                  child: ListView.builder(
-                    itemBuilder: (context, index) {
-                      int son = a();
-                      return Stack(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              height: 80,
-                              width: 80,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image: NetworkImage(data.pokemon[son].img)),
-                              ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: chiplar(),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Container(
+                height: 90.0,
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    int son = a();
+                    return Stack(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(18.0),
+                          child: Container(
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: NetworkImage(data.pokemon[son].img)),
                             ),
                           ),
-                          Positioned(
-                            child: Text("${data.pokemon[son].name}"),
-                            bottom: 0,
-                          ),
-                        ],
-                      );
-                    },
-                    itemCount: 7,
-                    scrollDirection: Axis.horizontal,
-                  ),
+                        ),
+                        Positioned(
+                          child: Text("${data.pokemon[son].name}"),
+                          bottom: 10,
+                        ),
+                      ],
+                    );
+                  },
+                  itemCount: 14,
+                  scrollDirection: Axis.horizontal,
                 ),
               ),
-            ],
+            ),
+          ],
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: Container(
+            margin: EdgeInsets.all(40.0),
+            width: 100.0,
+            height: 100.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50.0),
+              border: Border.all(
+                color: Colors.grey,
+                width: 1,
+              ),
+            ),
+            child: kelganPoke.nextEvolution != null
+                ? Padding(
+                    padding: const EdgeInsets.only(top: 38.0),
+                    child: Column(
+                      children: [
+                        Text("Next Level"),
+                        Text("${kelganPoke.nextEvolution[0].nameLevel}"),
+                      ],
+                    ),
+                  )
+                : Text("There is no Next Evolution"),
           ),
-        ],
-      ),
+        ),
+      ],
+    );
+  }
+
+  Widget stackWidgetPortrait(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          width: MediaQuery.of(context).size.width - 40,
+          height: MediaQuery.of(context).size.height - 300,
+          top: 20,
+          right: 20,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            elevation: 1,
+          ),
+        ),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Hero(
+            tag: kelganPoke.img,
+            child: Image.network(
+              kelganPoke.img,
+              fit: BoxFit.contain,
+              width: 200.0,
+            ),
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                "${kelganPoke.candy}",
+                style: TextStyle(fontSize: 25.0),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: chiplar(),
+            ),
+            kelganPoke.nextEvolution != null
+                ? Padding(
+                    padding: const EdgeInsets.all(38.0),
+                    child: ListTile(
+                      trailing: Icon(Icons.arrow_forward),
+                      title: Text("Next Level"),
+                      subtitle:
+                          Text("${kelganPoke.nextEvolution[0].nameLevel}"),
+                    ),
+                  )
+                : Text("There is no Next Evolution"),
+            Padding(
+              padding: const EdgeInsets.only(top: 48.0),
+              child: Container(
+                height: 100.0,
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    int son = a();
+                    return Stack(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: NetworkImage(data.pokemon[son].img)),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          child: Text("${data.pokemon[son].name}"),
+                          bottom: 0,
+                        ),
+                      ],
+                    );
+                  },
+                  itemCount: 7,
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 
--- lib/ui/pokemonList.dart
+++ lib/ui/pokemonList.dart
@@ -31,75 +31,161 @@ class _PokemonListState extends State<PokemonList> {
         title: Text("Pokemon App"),
       ),
 //https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json
-      body: Center(
-        child: FutureBuilder(
-          future: dataFromInternetVariable,
-          builder: (context, AsyncSnapshot snapshot) {
-            var data = snapshot.data;
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Platform.isAndroid
-                  ? CircularProgressIndicator()
-                  : CupertinoActivityIndicator();
-            } else {
-              return GridView.count(
-                crossAxisCount: 2,
-                children: data.pokemon.map<Widget>(
-                  (poke) {
-                    return InkWell(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return PokemonDetail(
-                                kelganPoke: poke,
-                                data: data,
-                              );
-                            },
-                          ),
-                        );
-                      },
-                      child: Hero(
-                        tag: poke.img,
-                        child: Container(
-                          margin: EdgeInsets.all(10.0),
-                          decoration: BoxDecoration(
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black,
-                                  blurRadius: 2,
-                                  offset: Offset(0.5, 0.10),
-                                )
-                              ],
-                              color: Colors.white,
-                              border:
-                                  Border.all(color: Colors.black, width: 0.5)),
-                          child: Column(
-                            children: [
-                              Container(
-                                width: 100.0,
-                                height: 100.0,
-                                child: FadeInImage.assetNetwork(
-                                  placeholder: 'assets/loading.gif',
-                                  image: poke.img != ''
-                                      ? poke.img
-                                      : 'https://cdn.dribbble.com/users/902865/screenshots/4814970/loading-opaque.gif',
-                                ),
-                              ),
-                              Text(
-                                "${poke.name}",
-                                style: TextStyle(fontSize: 20.0),
-                              ),
+      body: OrientationBuilder(
+        builder: (context, holat) {
+          if (holat == Orientation.portrait) {
+            return bodyQismiPortrait();
+          } else {
+            return bodyQismiLandscape();
+          }
+        },
+      ),
+    );
+  }
+
+  Widget bodyQismiLandscape() {
+    return Center(
+      child: FutureBuilder(
+        future: dataFromInternetVariable,
+        builder: (context, AsyncSnapshot snapshot) {
+          var data = snapshot.data;
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Platform.isAndroid
+                ? CircularProgressIndicator()
+                : CupertinoActivityIndicator();
+          } else {
+            return GridView.count(
+              childAspectRatio: 2,
+              crossAxisCount: 3,
+              children: data.pokemon.map<Widget>(
+                (poke) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return PokemonDetail(
+                              kelganPoke: poke,
+                              data: data,
+                            );
+                          },
+                        ),
+                      );
+                    },
+                    child: Hero(
+                      tag: poke.img,
+                      child: Container(
+                        margin: EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black,
+                                blurRadius: 2,
+                                offset: Offset(0.5, 0.10),
+                              )
                             ],
-                          ),
+                            color: Colors.white,
+                            border:
+                                Border.all(color: Colors.black, width: 0.5)),
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 100.0,
+                              height: 100.0,
+                              child: FadeInImage.assetNetwork(
+                                placeholder: 'assets/loading.gif',
+                                image: poke.img != ''
+                                    ? poke.img
+                                    : 'https://cdn.dribbble.com/users/902865/screenshots/4814970/loading-opaque.gif',
+                              ),
+                            ),
+                            Text(
+                              "${poke.name}",
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          ],
                         ),
                       ),
-                    );
-                  },
-                ).toList(),
-              );
-            }
-          },
-        ),
+                    ),
+                  );
+                },
+              ).toList(),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  Widget bodyQismiPortrait() {
+    return Center(
+      child: FutureBuilder(
+        future: dataFromInternetVariable,
+        builder: (context, AsyncSnapshot snapshot) {
+          var data = snapshot.data;
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Platform.isAndroid
+                ? CircularProgressIndicator()
+                : CupertinoActivityIndicator();
+          } else {
+            return GridView.count(
+              crossAxisCount: 2,
+              children: data.pokemon.map<Widget>(
+                (poke) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return PokemonDetail(
+                              kelganPoke: poke,
+                              data: data,
+                            );
+                          },
+                        ),
+                      );
+                    },
+                    child: Hero(
+                      tag: poke.img,
+                      child: Container(
+                        margin: EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black,
+                                blurRadius: 2,
+                                offset: Offset(0.5, 0.10),
+                              )
+                            ],
+                            color: Colors.white,
+                            border:
+                                Border.all(color: Colors.black, width: 0.5)),
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 100.0,
+                              height: 100.0,
+                              child: FadeInImage.assetNetwork(
+                                placeholder: 'assets/loading.gif',
+                                image: poke.img != ''
+                                    ? poke.img
+                                    : 'https://cdn.dribbble.com/users/902865/screenshots/4814970/loading-opaque.gif',
+                              ),
+                            ),
+                            Text(
+                              "${poke.name}",
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ).toList(),
+            );
+          }
+        },
       ),
     );
   }
